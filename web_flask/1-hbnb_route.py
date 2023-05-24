3bca1966cbc3f465d808f0882cd03e287debc2da
bayisa <bayisa2022@gmail.com>
2023-05-24 12:05:53 +0000

    Tsk1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A script that starts a flask web application """
+""" A script that starts a Flask Web Application """
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,6 +8,12 @@ app = Flask(__name__)
 def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
 if __name__ == "__main__":
     """ Main Function """
