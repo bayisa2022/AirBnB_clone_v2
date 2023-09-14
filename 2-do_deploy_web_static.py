9248b70f29ba322dee0a9503ed19f3a296ea14bf
bayisa <bayisa2022@gmail.com>
2023-09-14 11:16:25 +0000

    Task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
 
-from fabric.api import env, put, run, settings # Import Fabric functions
+"""Fabric script to deploy web_static to web servers."""
+
+from fabric.api import run, put, env, local
+from datetime import datetime
 import os
 
-# Set hosts to deploy to 
-env.hosts = ['<IP web-01>', '<IP web-02>']  
+env.hosts = ['54.87.250.211', '54.173.200.45']
+
+
+def do_pack():
+    """ Create a .tgz archive from the contents of web_static folder."""
+
+    m_date = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_path = "versions/web_static_{}.tgz".format(m_date)
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf {} web_static'.format(file_path))
+        return file_path
+    except:
+        return none
+
 
 def do_deploy(archive_path):
+    """Distribute the archive to the web servers."""
 
-  # Check if archive file exists
-  if not os.path.exists(archive_path):
-    return False
+    if archive_path is None or os.path.isfile(archive_path) is False:
+        return False
 
-  # Get archive name and dir name
-  archive_name = os.path.basename(archive_path) 
-  archive_dir = archive_name.split('.')[0]
-
-  # Wrap in error handling 
-  with settings(warn_only=True):
-    
-    # Upload archive to temp directory
-    put(archive_path, '/tmp/')  
-
-    # Create releases dir if needed
-    run('mkdir -p /data/web_static/releases/{}'.format(archive_dir))
-
-    # Uncompress archive
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
-      archive_name, archive_dir))
-
-    # Delete uploaded archive
-    run('rm /tmp/{}'.format(archive_name))
-
-    # Delete old symlink
-    run('rm -rf /data/web_static/current')
-
-    # Symlink new release
-    run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(
-      archive_dir))
-
-  # Print success message
-  print("New version deployed!") 
-  return True
+    try:
+        a_name = archive_path.split('/')[-1]
+        put('{}'.format(archive_path), '/tmp/{}'.format(a_name))
+        name_target = a_name.split('.')[0]
+        full_target = "/data/web_static/releases/{}".format(name_target)
+        run('mkdir -p {}'.format(full_target))
+        run("tar -xzf /tmp/{} -C {}".format(a_name, full_target))
+        run('rm /tmp/{}'.format(a_name))
+        run('mv {}/web_static/* {}/'.format(full_target, full_target))
+        run('rm -Rf {}/web_static'.format(full_target))
+        run('rm -Rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(full_target))
+        return True
+    except:
+        return False
