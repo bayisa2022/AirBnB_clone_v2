5af812d0a09ae6e8fa9b4ee40e03ee1821d65de3
bayisa <bayisa2022@gmail.com>
2023-09-22 18:23:36 +0000

    Task-1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
+""" This is the 2nd Flask setup script. """
+
 from flask import Flask
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def hello():
-    return 'Hello HBNB!'
+    """
+        Flask route at root (http://localhost:5000/).
+        Displays 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
 
-@app.route('/hbnb')  
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """
+        Flask route at /hbnb (http://localhost:5000/hbnb).
+        Displays 'HBNB'.
+    """
+    return "HBNB"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
