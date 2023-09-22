2e121bd44cde27a6c28444006584f6eff96d173c
bayisa <bayisa2022@gmail.com>
2023-09-22 18:38:48 +0000

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
+""" This is the 3rd Flask setup script. """
+
 from flask import Flask
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def hello():
-    return 'Hello HBNB!' 
+    """
+        Flask route at root.
+        Displays 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
 
-@app.route('/hbnb')
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """
+        Flask route at /hbnb.
+        Displays 'HBNB'.
+    """
+    return "HBNB"
 
-@app.route('/c/<text>')
+
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    text = text.replace('_', ' ')
-    return 'C ' + text
+    """
+        Flask route at /c/<text>.
+        Displays 'C + <text>'.
+    """
+    return "C {}".format(text.replace('_', ' '))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
